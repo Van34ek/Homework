7a20a6792fabeabb46d57c0d40c5991ae3fc48d6
Ivan <derzoman228@gmail.com>
2023-02-27 11:54:37 +0700

    задача 1

---
+++ for, while.py
@@ -0,0 +1,13 @@
+# n = int(input())
+# eagle = 0
+# tails = 0
+# for i in range(n):
+#     x = int(input())
+#     if x == 0:
+#         eagle += 1
+#     else:
+#         tails += 1
+# if eagle > tails:
+#     print(tails)
+# else:
+#     print(eagle)
