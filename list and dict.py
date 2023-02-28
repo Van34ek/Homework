5a24fb309dfe40365db9c53a3896abc329c86a6f
Ivan <derzoman228@gmail.com>
2023-02-28 14:01:51 +0700

    задача 3

--- list and dict.py
+++ list and dict.py
@@ -6,4 +6,16 @@
 # else:
 #     print('ничего')
 
-
+#задача 2
+# Задача 3
+list_English = {1:'AEIOULNSTR', 2:'DG', 3:'BCMP', 4:'FHVWY', 5:'K',
+               8: 'JX', 10: 'QZ'}
+list_Russia = {1:'АВЕТНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ', 4:'ЙЫ', 5: 'ЖЗХЦЧ',
+               8:'ШЭЮ', 10:'ФЩЪ'}
+word = input('Введите слова на английскои или на Русском: ')
+count = 0
+for i in word:
+    for keys, values in list_Russia.items():
+        if i in values:
+            count += keys
+print(f'Стоимость введённого слова равняется {count}')
